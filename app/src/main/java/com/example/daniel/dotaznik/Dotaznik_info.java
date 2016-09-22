cc62e549add99aa98bf5b5d7dc8557a47b10aa99
Jakub <Kubco332>
2016-09-22 14:49:29 +0200

    priprava na odosielanie dat

---
+++ app/src/main/java/com/example/daniel/dotaznik/Dotaznik_info.java
@@ -0,0 +1,96 @@
+package com.example.daniel.dotaznik;
+
+/**
+ * Created by Jakub on 22.09.2016.
+ */
+public class Dotaznik_info {
+    private String data;
+    private String data1;
+    private String data2;
+    private String data3;
+    private String data4;
+    private String data5;
+    private String data6;
+    private String data7;
+    private String data8;
+    private String data9;
+    private String data10;
+    private String data11;
+    private String data12;
+    private String data13;
+    private String data14;
+    private String data15;
+    private String data16;
+    private String data17;
+    private String data18;
+    private String data19;
+    private String data20;
+    private String data21;
+    private String data22;
+    private String data23;
+    private String data24;
+    private String data25;
+    private String data26;
+    private String data27;
+    private String data28;
+    private String data29;
+    private String data30;
+
+    //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    //constructors
+    //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    public Dotaznik_info()
+    {
+        data = "";data1 = "";data2 = "";data3 = "";data4 = "";data5 = "";data6 = "";data7 = "";data8 = "";data9 = "";data10 = "";
+        data11 = "";data12 = "";data13 = "";data14 = "";data15 = "";data16 = "";data17 = "";data18 = "";data19 = "";data20 = "";
+        data21 = "";data22 = "";data23 = "";data24 = "";data25 = "";data26 = "";data27 = "";data28 = "";data29 = "";data30 = "";
+    }
+
+    public Dotaznik_info(String sData,String sData1,String sData2,String sData3,String sData4,String sData5,String sData6,
+                         String sData7,String sData8,String sData9,String sData10,String sData11,String sData12,String sData13,
+                         String sData14,String sData15,String sData16,String sData17,String sData18,String sData19,String sData20,
+                         String sData21,String sData22,String sData23,String sData24,String sData25,String sData26,String sData27,String sData28,String sData29,String sData30)
+    {
+        data = sData;     data1 = sData1;   data2 = sData2;   data3 = sData3;   data4 = sData4;   data5 = sData5;   data6 = sData6;   data7 = sData7;   data8 = sData8;   data9 = sData9;   data10 = sData10;
+        data11 = sData11; data12 = sData12; data13 = sData13; data14 = sData14; data15 = sData15; data16 = sData16; data17 = sData17; data18 = sData18; data19 = sData19; data20 = sData21;
+        data21 = sData21; data22 = sData22; data23 = sData23; data24 = sData24; data25 = sData25; data26 = sData26; data27 = sData27; data28 = sData28; data29 = sData29; data30 = sData30;
+    }
+
+    //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    //getters
+    //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    public String getData(){return data;}       public String getData1(){return data1;}     public String getData2(){return data2;}
+    public String getData3(){return data3;}     public String getData4(){return data4;}     public String getData5(){return data5;}     public String getData6(){return data6;}
+    public String getData7(){return data7;}     public String getData8(){return data8;}     public String getData9(){return data9;}     public String getData10(){return data10;}
+    public String getData11(){return data11;}   public String getData12(){return data12;}   public String getData13(){return data13;}   public String getData14(){return data14;}
+    public String getData15(){return data15;}   public String getData16(){return data16;}   public String getData17(){return data17;}   public String getData18(){return data18;}
+    public String getData19(){return data19;}   public String getData20(){return data20;}   public String getData21(){return data21;}   public String getData22(){return data22;}
+    public String getData23(){return data23;}   public String getData24(){return data24;}   public String getData25(){return data25;}   public String getData26(){return data26;}
+    public String getData27(){return data27;}   public String getData28(){return data28;}   public String getData29(){return data29;}   public String getData30(){return data30;}
+
+    //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    //setters
+    //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    public void setData(String sData){data = sData;}            public void setData1(String sData1){data1 = sData1;}        public void setData2(String sData2){data2 = sData2;}
+    public void setData3(String sData3){data3 = sData3;}        public void setData4(String sData4){data4 = sData4;}        public void setData5(String sData5){data5 = sData5;}        public void setData6(String sData6){data6 = sData6;}
+    public void setData7(String sData7){data7 = sData7;}        public void setData8(String sData8){data8 = sData8;}        public void setData9(String sData9){data9 = sData9;}        public void setData10(String sData10){data10 = sData10;}
+    public void setData11(String sData11){data11 = sData11;}    public void setData12(String sData12){data12 = sData12;}    public void setData13(String sData13){data13 = sData13;}    public void setData14(String sData14){data14 = sData14;}
+    public void setData15(String sData15){data15 = sData15;}    public void setData16(String sData16){data16 = sData16;}    public void setData17(String sData17){data17 = sData17;}    public void setData18(String sData18){data18 = sData18;}
+    public void setData19(String sData19){data19 = sData19;}    public void setData20(String sData20){data20 = sData20;}    public void setData21(String sData21){data21 = sData21;}    public void setData22(String sData22){data22 = sData22;}
+    public void setData23(String sData23){data23 = sData23;}    public void setData24(String sData24){data24 = sData24;}    public void setData25(String sData25){data25 = sData25;}    public void setData26(String sData26){data26 = sData26;}
+    public void setData27(String sData27){data27 = sData27;}    public void setData28(String sData28){data28 = sData28;}    public void setData29(String sData29){data29 = sData29;}    public void setData30(String sData30){data30 = sData30;}
+
+    //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    //toString
+    //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    public String toString()
+    {
+       return data + "\t" + data1 + "\t" + data2 + "\t" + data3 + "\t" + data4 + "\t" + data5 + "\t" + data6 + "\t" + data7 + "\t" + data8 + "\t" + data9 + "\t" + data10 + "\t" +
+                data11 + "\t" + data12 + "\t" + data13 + "\t" + data14 + "\t" + data15 + "\t" + data16 + "\t" + data17 + "\t" + data18 + "\t" + data19 + "\t" + data20 + "\t" +
+                data21 + "\t" + data22 + "\t" + data23 + "\t" + data24 + "\t" + data25 + "\t" + data26 + "\t" + data27 + "\t" + data28 + "\t" + data29 + "\t" + data30 + "\t";
+    }
+
+}
