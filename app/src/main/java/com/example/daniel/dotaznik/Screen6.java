bcf2ed3cd26d126636ea3ef227b2763137b3c5f5
Daniel <danko.skala@gmail.com>
2016-10-02 14:37:46 +0200

    editTexty hotove, zvacsene, neda sa pisat enter aj zalomenie je ok, aspon na mojom mobile

--- app/src/main/java/com/example/daniel/dotaznik/Screen6.java
+++ app/src/main/java/com/example/daniel/dotaznik/Screen6.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.InputFilter;
+import android.text.Spanned;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -67,6 +70,7 @@ public class Screen6 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen6);
+
         Intent i = getIntent();
         String s = i.getStringExtra("ksc6");
         if (i.getStringExtra("ksc6") == null)
