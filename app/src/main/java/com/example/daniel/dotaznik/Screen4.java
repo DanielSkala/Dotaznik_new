4b39bb952a7f82654ab11a745acedec3b06158c2
Peter Grajcar <peter.grajcar131@gmail.com>
2016-10-10 21:10:32 +0200

    Added missing line

--- app/src/main/java/com/example/daniel/dotaznik/Screen4.java
+++ app/src/main/java/com/example/daniel/dotaznik/Screen4.java
@@ -170,7 +170,7 @@ public class Screen4 extends AppCompatActivity {
         ready.putExtra("sc27", toy.getStringExtra("sc27"));
         ready.putExtra("sc28", toy.getStringExtra("sc28"));
         ready.putExtra("macAddress", toy.getStringExtra("macAddress"));
-
+        ready.putExtra("name", toy.getStringExtra("name"));
 
 
 
