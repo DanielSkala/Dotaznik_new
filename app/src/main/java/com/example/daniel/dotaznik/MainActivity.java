057f5b87fa3be50b45c8627f5a98b019cba9ebb9
Jakub <Kubco332>
2016-10-16 22:51:41 +0200

    checkboxy

--- app/src/main/java/com/example/daniel/dotaznik/MainActivity.java
+++ app/src/main/java/com/example/daniel/dotaznik/MainActivity.java
@@ -76,9 +76,12 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
                     editor.putString("Q19_" + i, null);
                     editor.commit();
 
-                    name = ((EditText) findViewById(R.id.editText2)).getText().toString();
+
+                    EditText nameOfAnketer = (EditText) findViewById(R.id.editText2);
+                    nameOfAnketer.setText(getName());
+                    name = getName();
                     editor = getSharedPreferences("name", MODE_PRIVATE).edit();
-                    editor.putString("name", name);
+                    editor.putString("name", getName());
                     editor.commit();
 
                     if (i < 6) {
@@ -287,6 +290,40 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
             }
 
         return map.size();
+    }
+    public String getName() {
+
+        final TreeMap<Integer, Dotaznik_info> map = new TreeMap<Integer, Dotaznik_info>();
+        int iCounter = 0;
+
+        File root1 = android.os.Environment.getExternalStorageDirectory();
+        File dir1 = new File(root1.getAbsolutePath() + "/download");
+        File file1 = new File(dir1, "myData1.txt");
+
+
+        try {
+
+            BufferedReader bufferedReader1 = new BufferedReader(new InputStreamReader(new FileInputStream(file1), "UTF-8"), 8192);
+            String data;
+            while ((data = bufferedReader1.readLine()) != null) {
+                String get[] = data.split("\t");
+                map.put(iCounter, new Dotaznik_info(get[0], get[1], get[2], get[3], get[4], get[5], get[6], get[7], get[8], get[9], get[10],
+                        get[11], get[12], get[13], get[14], get[15], get[16], get[17], get[18], get[19], get[20],
+                        get[21], get[22], get[23], get[24], get[25], get[26], get[27], get[28], get[29], get[30],
+                        get[31], get[32], get[33], get[34], get[35], get[36], get[37], get[38], get[39], get[40], get[41], get[42], get[43], get[44], get[45]));
+                iCounter++;
+
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if(map.get(iCounter).getData45().equals("null") || map.get(iCounter).getData45() ==  null)
+            return "null";
+        else
+            return map.get(iCounter).getData45();
     }
 }
 
